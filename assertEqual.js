68d9b26c331df1c3fe52ab5a1ff1928091ae49b8
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-14 16:54:17 -0800

    assert equal w emoji

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return "❤️" + "Assertion Passed: " + actual + " === " + expected;
+  } else {
+    return "🛑" + "Assertion Failed: " + actual + " !== " + expected;
+  }
+};
+
+
+//TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1,1));
