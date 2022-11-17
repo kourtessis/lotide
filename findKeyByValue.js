b2ebcd6804ef6ce44eaf7ea06bb92e5c4bb4785a
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-16 19:20:32 -0800

    find key by value code

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value)  {
+  //It should scan the object and return the first key which contains the given value.
+  const keys = Object.keys(object);
+  for (const key of keys) {
+    if (value === object[key]) {
+      return key;
+    }
+  }
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
